623a4c7ed64bbb1831dc46180a05592e8e95eae2
nizam754 <m.nizam754@outlook.com>
2021-06-26 14:50:56 +0600

    Get Best Server

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 
 import speedtest
 
+#Create speedtest object
+test = speedtest.Speedtest()
+
+print("Server loading.....")
+
+
+#Get a list of servers
+test.get_servers()
+
+print ("Choosing ideal server...")
+
+ideal = test.get_best_server()
+
+print(ideal)
