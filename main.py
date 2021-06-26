7bd9ea0650f0641b3946b175b3c1de961645acfe
nizam754 <m.nizam754@outlook.com>
2021-06-26 15:00:32 +0600

    print download, upload & ping outcome

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ print ("Choosing ideal server...")
 ideal = test.get_best_server()
 
 print(ideal)
+
+#Call the download function
+print("Executing download test...")
+download_outcome = test.download()
+
+#Call the upload function
+print("Executing upload test...")
+upload_outcome = test.upload()
+
+ping_outcome = test.results.ping
+
+print(download_outcome)
+print(upload_outcome)
+print(ping_outcome)
